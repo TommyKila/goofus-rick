d43e800c09a670896b453d726e07d28f62b83852
Tommy Moncaco <103176871+TommyKila@users.noreply.github.com>
2022-10-07 10:41:08 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-const rightEyePos = document.querySelector('.right').getBoundingClientRect();
-const leftEyePos = document.querySelector('.left').getBoundingClientRect();
+const rightEye = document.querySelector('.right');
+const leftEye = document.querySelector('.left');
 const eyes = document.querySelectorAll('.eye');
-
-//calculate the center of both eyes
-const rightCenterX = (rightEyePos.left + rightEyePos.right) / 2;
-const rightCenterY = (rightEyePos.top + rightEyePos.bottom) / 2;
-const leftCenterX = (leftEyePos.left + leftEyePos.right) / 2;
-const leftCenterY = (leftEyePos.top + leftEyePos.bottom) / 2;
 
 document.addEventListener('mousemove', function (e) {
   const mouseX = e.clientX;
   const mouseY = e.clientY;
+  //calculate the center of both eyes
+  const rightEyePos = rightEye.getBoundingClientRect();
+  const leftEyePos = leftEye.getBoundingClientRect();
+  const rightCenterX = (rightEyePos.left + rightEyePos.right) / 2;
+  const rightCenterY = (rightEyePos.top + rightEyePos.bottom) / 2;
+  const leftCenterX = (leftEyePos.left + leftEyePos.right) / 2;
+  const leftCenterY = (leftEyePos.top + leftEyePos.bottom) / 2;
   //calculate the coordinate of the center between 2 eyes
   const anchorX = (rightCenterX + leftCenterX) / 2;
   const anchorY = (rightCenterY + leftCenterY) / 2;
+  console.log(anchorX, anchorY);
   const angleDeg = angle(mouseX, mouseY, anchorX, anchorY)
   eyes.forEach(function (eye) {
     eye.style.transform = `rotate(${90 + angleDeg}deg)`;
